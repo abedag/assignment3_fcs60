9daf327915cf8c62e7d35110d887ed34fc23b2b2
abedag <abedalrahman7117@gmail.com>
2025-02-27 17:32:52 +0200

    Edit <> Adding indentation for functions of vehicle (forgotten indentation

--- assignment3_fcs60.py
+++ assignment3_fcs60.py
@@ -8,14 +8,14 @@ class Vehicle:
         self._daily_rental_price = daily_rental_price
 
     
-def display_info(self):
-    print(f"Vehicle: {self.brand} {self.model}, Year: {self.year}, Daily Rental Price: {self._daily_rental_price}")
+    def display_info(self):
+        print(f"Vehicle: {self.brand} {self.model}, Year: {self.year}, Daily Rental Price: {self._daily_rental_price}")
 
-def calculate_rental_cost(self, days):
-    return self._daily_rental_price * days
+    def calculate_rental_cost(self, days):
+        return self._daily_rental_price * days
 
 class Car(Vehicle):
     def __init__(self, brand, model, year, daily_rental_price, seating_capacity):
         Vehicle.__init__(brand, model, year, daily_rental_price)
         self.seating_capacity = seating_capacity
-        
+
