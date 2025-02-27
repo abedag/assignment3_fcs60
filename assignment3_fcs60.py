cf43b3c058c646e7fa1a14ccd94084d9f7302ccb
abedag <abedalrahman7117@gmail.com>
2025-02-27 17:47:14 +0200

    Add <> Adding bike class that inherits vehicle's atributes (in addition of the engine capacity

--- assignment3_fcs60.py
+++ assignment3_fcs60.py
@@ -15,11 +15,14 @@ class Vehicle:
         return self._daily_rental_price * days
 
 class Car(Vehicle):
-    def __init__(self, brand, model, year, daily_rental_price, seating_capacity):
+    def __init__(self, brand, model, year, seating_capacity, daily_rental_price):
         Vehicle.__init__(brand, model, year, daily_rental_price)
         self.seating_capacity = seating_capacity
 
     def display_info(self):
         print(f"Car: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seating_capacity} Daily Rental Price: {self._daily_rental_price}")
 
-
+class Bike(Vehicle):
+    def __init__(self, brand, model, year, engine_capacity, daily_rental_price):
+        Vehicle.__init__(brand, model, year, daily_rental_price)
+        self.engine_capacity = engine_capacity
